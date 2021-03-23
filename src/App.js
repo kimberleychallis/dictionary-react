a65b5343c3abe12642499d0c9a5afa640260623c
Kimberley Challis <kimchallis@gmail.com>
2021-03-23 13:58:47 +0000

    Added basic navigation and search bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,35 @@ import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
+    <div className="container">
+      <div className="App">
         <header className="App-header">
-          <p>Hello, world!</p>
+          <div className="row">
+            <div className="col-3">
+              <h1>dictionary.js</h1>
+            </div>
+            <div className="col-9 menubar">
+              <ul>
+                <li>
+                  <a href="#">source code</a>
+                </li>
+                <li>
+                  <a href="#">inspired by</a>
+                </li>
+                <li>
+                  <a href="#">about</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </header>
+        <main className="App-body">
+          <h2>what would you like to understand?</h2>
+          <form>
+            <input type="text" placeholder="" />
+            <input type="submit" value="search" />
+          </form>
+        </main>
       </div>
     </div>
   );
