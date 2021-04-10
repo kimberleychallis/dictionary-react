f35f2593c3c3bd40a6426e612fa7bdd828e0597b
Kimberley Challis <kimchallis@gmail.com>
2021-04-10 23:18:56 +0100

    Mapped definitions

--- src/Meaning.js
+++ src/Meaning.js
@@ -11,74 +11,56 @@ const Meaning = (props) => {
           <div className="col-2 partOfSpeech">
             <p>{props.resultDetails.partOfSpeech}</p>
           </div>
-          <div className="col col-md-6 entry">
-            {/* BEGINNING OF A DEFINITION */}
 
+          {/* BEGINNING OF A DEFINITION */}
+
+          <div className="col-6 entry">
             {props.resultDetails.definitions.map((definition, index) => {
               return (
                 <div key={index}>
                   <h4 className="definition">
                     {index + 1}. {definition.definition}
                   </h4>
+
+                  <div className="row synonyms">
+                    <div className="col-2">
+                      <h5>synonyms</h5>
+                    </div>
+                    <div className="col-9">
+                      <ul>
+                        <li>slot</li>
+                        <li>arena</li>
+                        <li>capacity</li>
+                        <li>zone</li>
+                        <li>area</li>
+                        <li>field</li>
+                        <li>location</li>
+                        <li>territory</li>
+                        <li>area</li>
+                        <li>field</li>
+                        <li>location</li>
+                        <li>territory</li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
               );
             })}
-
-            {/* <div className="row synonyms">
-              <div className="col-2">
-                <h5>synonyms</h5>
-              </div>
-              <div className="col-9">
-                <ul>
-                  <li>slot</li>
-                  <li>arena</li>
-                  <li>capacity</li>
-                  <li>zone</li>
-                  <li>area</li>
-                  <li>field</li>
-                  <li>location</li>
-                  <li>territory</li>
-                  <li>area</li>
-                  <li>field</li>
-                  <li>location</li>
-                  <li>territory</li>
-                </ul>
-              </div>
-              }
-            </div>
           </div>
+
+          {/* EXAMPLE PHRASES */}
+
           <div className="col col-md-4 phrases">
             <p className="-small-caps">example</p>
             <p className="example">
               &ldquo;
-              {props.resultDetails.definitions[0].example}
+              {/* {props.resultDetails.definitions[0].example} */}
               &rdquo;
             </p>
-          </div> */}
-
-            {/* SYNONYMS */}
-
-            {/* <div className="row synonyms">
-            <div className="col-2">
-              <h5>synonyms</h5>
-            </div>
-            <div className="col-9">
-              <ul>
-                <li>period</li>
-                <li>span</li>
-                <li>time</li>
-                <li>duration</li>
-                <li>stretch</li>
-                <li>course</li>
-                <li>interval</li>
-                <li>season</li>
-                <li>term</li>
-              </ul>
-            </div>
-          </div> */}
-
-            {/* END OF SYNONYMS */}
           </div>
+
+          {/* END EXAMPLE PHRASES */}
+          {/* END OF MAPPING */}
         </div>
       </div>
     </div>
